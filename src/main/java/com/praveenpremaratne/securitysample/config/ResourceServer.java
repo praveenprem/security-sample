b1394bdbf27faeea03886c9dd7045c615417dab6
Praveen Premaratne <praveen.premaratne@groupcdg.com>
2017-11-05 15:46:52 +0000

    git -c commit.gpgsign=false commit

---
+++ src/main/java/com/praveenpremaratne/securitysample/config/ResourceServer.java
@@ -0,0 +1,31 @@
+package com.praveenpremaratne.securitysample.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.error.OAuth2AccessDeniedHandler;
+
+@Configuration
+@EnableResourceServer
+public class ResourceServer extends ResourceServerConfigurerAdapter{
+
+    private String RESOURCE_ID = "SPRING_REST_API";
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        resources.resourceId(RESOURCE_ID).stateless(false);
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+                .and()
+                .authorizeRequests().antMatchers("/users").authenticated()
+                .and()
+                .exceptionHandling().accessDeniedHandler(new OAuth2AccessDeniedHandler());
+    }
+}
